928d675df75b6e94bae0f37f1e6af91fab36120e
Sandra Assunção <sandraassuncaosantos@gmail.com>
2026-08-07 10:51:41 -0300

    Update padronizacao_nome_cliente.py

--- 01_manipulacao_strings/padronizacao_nome_cliente.py
+++ 01_manipulacao_strings/padronizacao_nome_cliente.py
@@ -1,4 +1,15 @@
+# Solução para Padronização e Análise de Nome do Cliente
+
 nome = str(input('Nome completo do cliente: ')).strip()
-print(nome.upper())
-print(nome.title())
-print('O nome completo do cliente tem: {} letras'.format(len(nome) - nome.count(' ')))
+
+print('\n--- Resultados ---')
+print(f'Nome em maiúsculas: {nome.upper()}')
+print(f'Nome formatado: {nome.title()}')
+
+# Abordagem 1: Lógica Matemática (Tamanho total - quantidade de espaços)
+letras_matematica = len(nome) - nome.count(' ')
+print(f'Total de letras (via cálculo matemático): {letras_matematica}')
+
+# Abordagem 2: Manipulação de Texto (Separa em lista e junta sem espaços)
+letras_string = len(''.join(nome.split()))
+print(f'Total de letras (via manipulação de strings): {letras_string}')
