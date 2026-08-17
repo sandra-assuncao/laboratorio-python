4f5e1f37df1b67d10d97df7d5e17caa5d5fc4258
Sandra Assunção <sandraassuncaosantos@gmail.com>
2026-08-17 14:09:06 -0300

    Update alcada_aprovacao_contas_a_pagar.py

--- 02_condicionais_aplicadas_financas/alcada_aprovacao_contas_a_pagar.py
+++ 02_condicionais_aplicadas_financas/alcada_aprovacao_contas_a_pagar.py
@@ -1,3 +1,10 @@
+## A regra de negócio: O usuário informa o valor total de um lote de pagamentos (TED/Tributos/Fornecedores)
+## O if/else: O script verifica os limites da empresa:
+## Até R$ 50.000 → Aprovação automática pelo Analista;
+## Acima de R$ 50.000 → Requer dupla aprovação do Gerente de Tesouraria;
+## Acima do limite da conta bancária → Alerta de Saldo Insuficiente / Risco de Descoberto.
+
+
 nome_fornecedor = str(input('Digite o nome do fornecedor: '))
 print('Fornecedor: {}'.format(nome_fornecedor))
 valor_a_pagar = float(input('Digite o valor do pagamento do título R$: '))
